119a44866de1b56c60c07ad1f93f6ccc820d48e7
gbagul <gbagul@avaya.com>
2022-07-18 12:17:39 +0530

    angular-tutorial

---
+++ src/app/hello-world.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hello-world',
+  template: '<h1>{{title}}</h1>',
+  styles: [
+    `
+      h1 {
+        color: red;
+      }
+    `,
+  ],
+})
+export class HelloWorldComponent {
+  title = 'Hello World';
+}
